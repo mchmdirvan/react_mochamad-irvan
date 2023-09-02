cfe7e190f5b59e1f6113824dbc5b14f4b0a80fe4
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-02 16:02:40 +0700

    feat: completely validation form features where productName not exceed 25 character,product name and product price can't be null,product name not have to special character, appear message where the form is null, control button when all form valid or invalid, coloring all forms input

--- 07_Javascript-Refreshment/Praktikum/assets/js/index.js
+++ 07_Javascript-Refreshment/Praktikum/assets/js/index.js
@@ -3,20 +3,38 @@ function validateForm(event) {
   const form = event.target;
   const productNameInput = form.querySelector("#productName");
   const productNameError = form.querySelector("#productNameError");
+  const invalidCharactersPattern = /[{}@#%]/;
 
   if (!form.checkValidity()) {
     event.preventDefault();
     event.stopPropagation();
+
+    //control button
+    const submitButton = form.querySelector("#submitButton");
+    submitButton.setAttribute("disabled", "true");
+  } else {
+    //control button
+    const submitButton = form.querySelector("#submitButton");
+    submitButton.removeAttribute("disabled");
   }
+  //check the length of product name
   if (productNameInput.value.length > 25) {
     productNameError.textContent =
       "Product Name must not exceed 25 characters.";
+    productNameInput.classList.add("is-invalid");
+    // add manually border red
+    productNameInput.classList.add("red");
+    event.preventDefault();
+    
+    // check symbol on product name
+  } else if (invalidCharactersPattern.test(productNameInput.value)) {
+    productNameError.textContent = "Name must not contain symbols.";
+    productNameInput.classList.add("is-invalid");
     productNameInput.classList.add("red");
     event.preventDefault();
   } else {
-    productNameError.textContent = "Please enter a valid Prouct name";
+    productNameError.textContent = "Please enter a valid Product Name";
   }
-
   form.classList.add("was-validated");
 }
 
