026902b7308fa26dc88442b219c3b3a5234788e7
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-16 22:28:51 +0700

    refactor: Index.jsx into CreateProduct.jsx

--- 12_React-Fundametal/Praktikum/src/main.jsx
+++ 12_React-Fundametal/Praktikum/src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Index from './pages/Index'
-import './styles/style.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CreateProduct from "./pages/CreateProduct";
+import "./styles/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Index/>
-  </React.StrictMode>,
-)
+    <CreateProduct />
+  </React.StrictMode>
+);
