33f8eb8aa36b7bb255911d019a52e994b1a85027
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-16 20:50:02 +0700

    refactor: modify Vite template and create Index.jsx in pages folder

--- 12_React-Fundametal/Praktikum/src/main.jsx
+++ 12_React-Fundametal/Praktikum/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Index from './pages/Index'
+import './styles/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Index/>
   </React.StrictMode>,
 )
---
+++ 12_React-Fundametal/Praktikum/src/pages/Index.jsx
@@ -0,0 +1,9 @@
+function Index() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
+}
+
+export default Index;
