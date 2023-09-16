adcd9e6e14fabae66eddf8a92bb6a2177c7b6948
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-16 22:28:40 +0700

    refactor: Index.jsx into CreateProduct.jsx

--- 12_React-Fundametal/Praktikum/src/pages/CreateProduct.jsx
+++ 12_React-Fundametal/Praktikum/src/pages/CreateProduct.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Navbar from "../components/Navbar";
+import MainContent from "../components/MainContent";
 
-function Index() {
+function CreateProduct() {
   return (
     <>
       <Navbar />
+      <MainContent />
     </>
   );
 }
 
-export default Index;
+export default CreateProduct;
