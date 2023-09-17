c5afc9cc5d74b3bfba567ae484d2f1f5a6be05c4
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-17 14:07:54 +0700

    feat: create article.js for createAccount.jsx

--- 13_Event-Handling/Praktikum/src/main.jsx
+++ 13_Event-Handling/Praktikum/src/main.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CreateProduct from "./pages/CreateProduct";
 import "./styles/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import CreateAccount from "./pages/CreateAccount";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CreateProduct />
+    <CreateAccount />
   </React.StrictMode>
 );
