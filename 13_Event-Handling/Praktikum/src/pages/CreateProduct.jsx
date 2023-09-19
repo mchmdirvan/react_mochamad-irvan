796f5d37ba95573d16b96e5f0bb7b9160bdf2b84
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-19 12:17:38 +0700

    refactor: move herosection into CreateProduct

--- 13_Event-Handling/Praktikum/src/pages/CreateProduct.jsx
+++ 13_Event-Handling/Praktikum/src/pages/CreateProduct.jsx
@@ -3,13 +3,29 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import MainForm from "../components/MainForm";
 import Table from "../components/Table";
-import HeroSection from "../components/HeroSection";
+import BootstrapLogo from "../assets/bootstrap-logo.svg";
 
 function CreateProduct() {
   return (
     <>
       <Navbar />
-      <HeroSection />
+
+      <main className="body">
+        {/* Title and Description */}
+        <img
+          src={BootstrapLogo}
+          alt="Bootstrap Logo"
+          className="mx-auto d-block bootstrap-logo"
+        />
+
+        <h2>Create Product</h2>
+        <p>
+          Below is an example form built entirely with Bootstrap’s form
+          controls. Each required form group has a validation state that can be
+          triggered by attempting to submit the form without completing it.
+        </p>
+      </main>
+
       <MainForm />
       <Table />
     </>
