c1fd15ecea467844f2327ffe9f9dc685cee2f8e4
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-26 14:37:31 +0700

    feat: adding navbar component

---
+++ 15_React-Routing/Praktikum/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function LandingPage (){
+    return (
+        <>
+        <Navbar/>
+        </>
+    )
+}
+
+export default LandingPage
