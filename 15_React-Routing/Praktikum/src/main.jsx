be2ec4a79e0e2e43167db38dee48404296bb1cfe
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-27 08:49:58 +0700

    style: just moving position import

--- 15_React-Routing/Praktikum/src/main.jsx
+++ 15_React-Routing/Praktikum/src/main.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import ReactDOM from "react-dom/client";
-import CreateProduct from "./pages/CreateProduct";
-import "./styles/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+import CreateProduct from "./pages/CreateProduct";
 import CreateAccount from "./pages/CreateAccount";
 import LandingPage from "./pages/LandingPage";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
