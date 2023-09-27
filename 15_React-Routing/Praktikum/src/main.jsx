aa79c9f36436b536fdfe078e17781eb0879d9aa7
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-27 09:43:17 +0700

    refactor: deleting import page

--- 15_React-Routing/Praktikum/src/main.jsx
+++ 15_React-Routing/Praktikum/src/main.jsx
@@ -3,13 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom/client";
 import React from "react";
 
-import CreateProduct from "./pages/CreateProduct";
-import CreateAccount from "./pages/CreateAccount";
-import LandingPage from "./pages/LandingPage";
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 );
