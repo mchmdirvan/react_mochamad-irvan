87d2c334fd1d02a755926f2e5ca02d69ba5c8c7d
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-28 20:39:20 +0700

    style: styling the table with daisyUI hover

--- 15_React-Routing/Praktikum/src/components/Table.jsx
+++ 15_React-Routing/Praktikum/src/components/Table.jsx
@@ -5,55 +5,53 @@ import { BsFillPencilFill, BsFillTrash2Fill } from "react-icons/bs";
 
 function Table({ headers = [], datas = [], onEditClick, onDeleteClick }) {
   return (
-    <section className="row justify-content-center mt-5 table-section">
-      <div className="col-10">
-        <table className=" table table-hover table-bordered text-center col-10">
-          <thead className="table-dark">
-            <tr>
-              {headers.map((headers, index) => (
-                <th key={index} scope="col">
-                  {headers}
-                </th>
-              ))}
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {datas.map((data) => (
-              <tr key={data.id}>
-                <td>{data.id}</td>
-                <td>{data.productName}</td>
-                <td>{data.productCategory}</td>
-                <td>{data.productFreshness}</td>
-                <td>{data.productPrice}</td>
-                <td className="flex gap-2">
-                  <BsFillPencilFill onClick={() => onEditClick(data)} />
-                  <BsFillTrash2Fill onClick={() => onDeleteClick(data)} />
-                </td>
-              </tr>
+    <div className="overflow-x-auto px-[10rem] mt-10">
+      <table className="table">
+        <thead className="">
+          <tr>
+            {headers.map((headers, index) => (
+              <th key={index} scope="col">
+                {headers}
+              </th>
             ))}
-          </tbody>
-        </table>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {datas.map((data) => (
+            <tr className="hover" key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.productName}</td>
+              <td>{data.productCategory}</td>
+              <td>{data.productFreshness}</td>
+              <td>{data.productPrice}</td>
+              <td className="flex gap-2">
+                <BsFillPencilFill onClick={() => onEditClick(data)} />
+                <BsFillTrash2Fill onClick={() => onDeleteClick(data)} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-        {/* Buttons (Delete and Search) */}
-        <div className="d-flex flex-row gap-2">
-          <button
-            type="submit"
-            className="btn btn-danger col-1"
-            id="deleteButton"
-          >
-            Delete
-          </button>
-          <button
-            className="btn btn-outline-success"
-            type="submit"
-            id="searchButton"
-          >
-            Search
-          </button>
-        </div>
+      {/* Buttons (Delete and Search) */}
+      <div className="">
+        <button
+          type="submit"
+          className=""
+          id="deleteButton"
+        >
+          Delete
+        </button>
+        <button
+          className=""
+          type="submit"
+          id=""
+        >
+          Search
+        </button>
       </div>
-    </section>
+    </div>
   );
 }
 
