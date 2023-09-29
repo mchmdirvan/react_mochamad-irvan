d7527d6789247839e98082fb4fcbf7decb51255d
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-29 13:55:59 +0700

    feat: create handle on click to navigate to create product detail using useNavigate react-dom

--- 15_React-Routing/Praktikum/src/components/Table.jsx
+++ 15_React-Routing/Praktikum/src/components/Table.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
 import { BsFillPencilFill, BsFillTrash2Fill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import React from "react";
 
 function Table({ headers = [], datas = [], onEditClick, onDeleteClick }) {
+  const navigate = useNavigate();
+
   return (
     <div className="overflow-x-auto px-[10rem] mt-10">
       <table className="table">
@@ -19,7 +23,11 @@ function Table({ headers = [], datas = [], onEditClick, onDeleteClick }) {
         </thead>
         <tbody>
           {datas.map((data) => (
-            <tr className="hover" key={data.id}>
+            <tr
+              className="hover"
+              key={data.id}
+              onClick={() => navigate(`/create-product/${data.id}`)}
+            >
               <td>{data.id}</td>
               <td>{data.productName}</td>
               <td>{data.productCategory}</td>
