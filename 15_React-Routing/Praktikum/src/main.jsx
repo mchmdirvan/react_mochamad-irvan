66822ab85267673d5d1d994c9b00a5cb3ea87baf
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-29 07:23:00 +0700

    feat: cd running the main using react routes

--- 15_React-Routing/Praktikum/src/main.jsx
+++ 15_React-Routing/Praktikum/src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 
 import './styles/index.css';
-import LandingPage from "./pages/LandingPage";
+import App from './routes/index'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LandingPage/>
+    <App/>
   </React.StrictMode>
 );
