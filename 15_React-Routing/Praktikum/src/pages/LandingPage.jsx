b109799c18ef34719015b6371319a0b9790720d2
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-09-29 07:20:11 +0700

    refactor: deleting button go to create product to navbar

--- 15_React-Routing/Praktikum/src/pages/LandingPage.jsx
+++ 15_React-Routing/Praktikum/src/pages/LandingPage.jsx
@@ -35,24 +35,28 @@ function LandingPage() {
           <button className="ms-12 px-4 hover:border-white hover:rounded-full hover:border-2">
             Watch Video
           </button>
-          <button className="ms-12 px-4 hover:border-white hover:rounded-full hover:border-2">
-            Go to Create Product
-          </button>
         </div>
         <div className="">
           <img src={HeroImage} alt="" className="" />
         </div>
       </main>
 
-      
       <section className="flex flex-col items-center justify-center bg-[#F3F5FA] h-[35vh] font-[Jost] gap-3">
-        <h4 className="text-2xl font-medium text-[#37517E]">Join Our Newsletter</h4>
+        <h4 className="text-2xl font-medium text-[#37517E]">
+          Join Our Newsletter
+        </h4>
         <p>
           Tamen quem nulla quae legam multos aute sint culpa legam noster magna
         </p>
         <div className="">
-          <input className=" w-[30rem] rounded-full p-2 shadow-sm" type="email" required="" />
-          <button className=" rounded-full bg-[#47B2E4;] text-white p-2 px-5 shadow-sm">Subscribe</button>
+          <input
+            className=" w-[30rem] rounded-full p-2 shadow-sm"
+            type="email"
+            required=""
+          />
+          <button className=" rounded-full bg-[#47B2E4;] text-white p-2 px-5 shadow-sm">
+            Subscribe
+          </button>
         </div>
       </section>
       <Footer />
