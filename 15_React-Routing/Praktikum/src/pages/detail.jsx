b7c4107dadb8a00187de5387374c420e10f10b9e
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-03 14:54:07 +0700

    feat: create function to get data in local storage with props key and converting to object js and get the data by find method

--- 15_React-Routing/Praktikum/src/pages/detail.jsx
+++ 15_React-Routing/Praktikum/src/pages/detail.jsx
@@ -6,12 +6,24 @@ import { useTitle } from "../utils/hooks/customHooks";
 
 export default function Detail() {
   useTitle("Detail Data");
-  const { id} = useParams();
+
+  function getDataFromLocalStorage(key) {
+    const data = localStorage.getItem(key);
+    return JSON.parse(data);
+  }
+
+  const { id } = useParams();
+  const products = getDataFromLocalStorage("products") || [];
+  const product = products.find((product) => product.id === id);
 
   return (
     <div>
-      <h2>Detail Page</h2>
-      <p>ID: {id}</p>
+      <h2>Product Detail</h2>
+      <p>ID: {product.id}</p>
+      <p>Product Name: {product.productName}</p>
+      <p>Product Category: {product.productCategory}</p>
+      <p>Product Freshness: {product.productFreshness}</p>
+      <p>Product Price: {product.productPrice}</p>
     </div>
   );
 }
