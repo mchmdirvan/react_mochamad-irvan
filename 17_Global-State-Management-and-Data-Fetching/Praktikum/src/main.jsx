d00be841ece705b9da8752c39ca6a822895c434a
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-07 14:50:31 +0700

    feat: put provider store for redux

--- 17_Global-State-Management-and-Data-Fetching/Praktikum/src/main.jsx
+++ 17_Global-State-Management-and-Data-Fetching/Praktikum/src/main.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import React from "react";
 
-import './styles/index.css';
-import App from './routes/index'
+import store from "./utils/states/redux/store/store";
+import "./styles/index.css";
+import App from "./routes/index";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App/>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
