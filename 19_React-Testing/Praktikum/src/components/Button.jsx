1d760564b2b4093bf82d314eedae893d9e3467f0
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-08 23:33:20 +0700

    refactor: deletin unused props for clean code

--- 19_React-Testing/Praktikum/src/components/Button.jsx
+++ 19_React-Testing/Praktikum/src/components/Button.jsx
@@ -4,23 +4,20 @@ import React from "react";
 
 /**
  * Button
- * @param {{ 
- * type : button
+ * @param {{
+ * type : string
  * id : string
  * label : string
- * onClick : function onClick(params) {
- } 
  * }}
  */
 
-function Button({ type, id, label, onClick }) {
+function Button({ type, id, label }) {
   return (
     <div className="mt-5">
       <button
         className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-700"
         type={type}
         id={id}
-        onClick={onClick}
       >
         {label}
       </button>
