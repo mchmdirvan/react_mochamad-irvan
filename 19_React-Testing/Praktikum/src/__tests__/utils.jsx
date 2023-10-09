d4d21bb2b2597ec37ad57b5997c016a691ae4e88
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-09 11:06:38 +0700

    feat: create configuration for react testing

--- 19_React-Testing/Praktikum/src/__tests__/utils.jsx
+++ 19_React-Testing/Praktikum/src/__tests__/utils.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
