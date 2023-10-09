56b47db2f356e87245d2e9f0c95bd52685b3d36a
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-09 20:14:43 +0700

    feat: adding testing for render form components, and render product name input and displays the text value

--- 19_React-Testing/Praktikum/src/__tests__/pages/CreateProduct.test.jsx
+++ 19_React-Testing/Praktikum/src/__tests__/pages/CreateProduct.test.jsx
@@ -1,7 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import "@testing-library/jest-dom";
 import { render, screen, within, fireEvent } from "../utils";
 
 import App from "../../pages/CreateProduct";
+import { describe } from "vitest";
+
+const productInput = [
+  "input-product-name",
+  "input-product-category",
+  "input-product-image",
+  "input-product-freshness",
+  "input-product-description",
+  "input-product-price",
+  "btn-submit",
+];
 
 describe("Index Product Page", () => {
   beforeEach(() => {
@@ -10,7 +23,21 @@ describe("Index Product Page", () => {
   describe("Renders the page", () => {
     it("should render the form components", () => {
       const form = screen.getByLabelText("product-form");
-      expect(within(form).getByLabelText("input-product-name")).toBeTruthy();
+      for (const input of productInput) {
+        expect(within(form).getByLabelText(input)).toBeTruthy();
+      }
+    });
+  });
+
+  describe("Action for Product", () => {
+    it("render product name input and displays entered text", () => {
+      const form = screen.getByLabelText("product-form");
+      const productNameInput =
+        within(form).getByLabelText("input-product-name");
+      fireEvent.change(productNameInput, {
+        target: { value: "New Product" },
+      });
+      expect(productNameInput).toHaveValue("New Product");
     });
   });
 });
