c823ccf30dcd388f77907d62f008b93b7c6f491d
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-09 11:05:50 +0700

    feat: create configuration for react testing

---
+++ 19_React-Testing/Praktikum/src/__tests__/utils.jsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "src/utils/states/redux/store/store";
+
+const Providers = ({ children }) => {
+  return (
+    <BrowserRouter>
+      <Provider store={store}>{children}</Provider>
+    </BrowserRouter>
+  );
+};
+
+const customRender = (component) => {
+  return render(component, {
+    wrapper: Providers,
+  });
+};
+
+export * from "@testing-library/react";
+export { customRender as Render };
