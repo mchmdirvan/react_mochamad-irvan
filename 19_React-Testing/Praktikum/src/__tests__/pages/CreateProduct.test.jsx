72524f69dc390788a952561aec5061b768142a6f
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-11 11:29:09 +0700

    feat: testing to save and display all the form

--- 19_React-Testing/Praktikum/src/__tests__/pages/CreateProduct.test.jsx
+++ 19_React-Testing/Praktikum/src/__tests__/pages/CreateProduct.test.jsx
@@ -3,8 +3,7 @@
 import "@testing-library/jest-dom";
 import { render, screen, within, fireEvent } from "../utils";
 
-import App from "../../pages/CreateProduct";
-import { describe } from "vitest";
+import CreateProduct from "../../pages/CreateProduct";
 
 const productInput = [
   "input-product-name",
@@ -18,9 +17,9 @@ const productInput = [
 
 describe("Index Product Page", () => {
   beforeEach(() => {
-    render(<App />);
+    render(<CreateProduct />);
   });
-  describe("Renders the page", () => {
+  describe("Renders the form", () => {
     it("should render the form components", () => {
       const form = screen.getByLabelText("product-form");
       for (const input of productInput) {
@@ -29,15 +28,77 @@ describe("Index Product Page", () => {
     });
   });
 
-  describe("Action for Product", () => {
-    it("render product name input and displays entered text", () => {
+  describe("Input Actions", () => {
+    it("should allow entering and displaying text in the 'Product Name' input", () => {
       const form = screen.getByLabelText("product-form");
       const productNameInput =
         within(form).getByLabelText("input-product-name");
+
       fireEvent.change(productNameInput, {
         target: { value: "New Product" },
       });
       expect(productNameInput).toHaveValue("New Product");
+    });
+
+    it("should save and display Product Category", () => {
+      const form = screen.getByLabelText("product-form");
+      const productCategorySelect = within(form).getByLabelText(
+        "input-product-category"
+      );
+
+      fireEvent.change(productCategorySelect, {
+        target: { value: "Fruits" },
+      });
+      expect(productCategorySelect).toHaveValue("Fruits");
+    });
+
+    it("should save and display Product Image", () => {
+      const form = screen.getByLabelText("product-form");
+      const productImage = within(form).getByLabelText("input-product-image");
+
+      fireEvent.change(productImage, {
+        target: {
+          files: [
+            new File(["sample-image"], "sample.png", { type: "image/png" }),
+          ],
+        },
+      });
+      expect(productImage).toBeInTheDocument([
+        new File(["sample-image"], "sample.png", { type: "image/png" }),
+      ]);
+    });
+
+    it("should save and display Product Freshness", () => {
+      const form = screen.getByLabelText("product-form");
+      const productFreshness = within(form).getByLabelText(
+        "input-product-freshness"
+      );
+
+      fireEvent.click(productFreshness);
+    });
+
+    it("should save and display Product Description", () => {
+      const form = screen.getByLabelText("product-form");
+      const productDescription = within(form).getByLabelText(
+        "input-product-description"
+      );
+
+      fireEvent.change(productDescription, {
+        target: { value: "New Description" },
+      });
+      expect(productDescription).toHaveValue("New Description");
+    });
+
+    it("should save and display Product Price", () => {
+      const form = screen.getByLabelText("product-form");
+      const productPrice = within(form).getByLabelText(
+        "input-product-price"
+      );
+
+      fireEvent.change(productPrice, {
+        target: { value: "10" },
+      });
+      expect(productPrice).toBeInTheDocument("10");
     });
   });
 });
