a8c675c9cfad1344bc70a64e7e08d4d0eb0fe356
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-12 10:43:35 +0700

    feat: call API url with axiosWithConfig in router using useEffect

--- 21_Introduction-Restfull-API/Praktikum/src/routes/index.jsx
+++ 21_Introduction-Restfull-API/Praktikum/src/routes/index.jsx
@@ -1,11 +1,17 @@
 /* eslint-disable no-unused-vars */
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
 
+import { setAxiosConfig } from "../utils/API/axiosWithConfig";
 import CreateProduct from "../pages/CreateProduct";
 import LandingPage from "../pages/LandingPage";
 import DetailData from "../pages/detail";
 
 function Router() {
+  useEffect(() => {
+    setAxiosConfig("", "https://651a7c44340309952f0d5ec8.mockapi.io/api/v1/");
+  }, []);
+
   const router = createBrowserRouter([
     {
       path: "/",
