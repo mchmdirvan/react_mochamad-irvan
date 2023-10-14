25bacff66848e18c2dcc042c31d2da315e8a7443
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-14 07:31:09 +0700

    docs: copy project from restfull api

---
+++ 23_Authentication-in-React/Praktikum/src/components/Navbar.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import {Link} from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav className=" dark:bg-gray-900">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-2 shadow-md pr-10">
+        <a href="#" className="flex items-start">
+          <span className="self-center text-2xl  font-semibold whitespace-nowrap dark:text-white">
+            Simple Header
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link
+                href="#"
+                className=" bg-blue-600 text-white px-5 py-2 rounded-md hover:text-black hover:bg-white"
+                aria-current="page" to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <a href="#" className=" text-blue-600 hover:text-black">
+                Features
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-blue-600  hover:text-black">
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-blue-600  hover:text-black">
+                FAQs
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-blue-600  hover:text-black">
+                About
+              </a>
+            </li>
+            <li>
+              <Link href="#" className="text-blue-600  hover:text-black px-2 rounded-full hover:border-2" to="/create-product">
+                Create Product
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ 23_Authentication-in-React/Praktikum/src/utils/API/products/api.js
@@ -0,0 +1,61 @@
+import { v4 as uuidv4 } from "uuid";
+
+import axiosWithConfig from "../axiosWithConfig";
+
+export const getProducts = async () => {
+  try {
+    const response = await axiosWithConfig.get("/products");
+    return response.data;
+  } catch (error) {
+    throw Error("Failed to get products");
+  }
+};
+
+export const getDetailProducts = async (id) => {
+  try {
+    const response = await axiosWithConfig.get("/products/" + id);
+    return response.data;
+  } catch (error) {
+    throw Error("Failed to get a product");
+  }
+};
+
+export const createProduct = async (data) => {
+  try {
+    const newData = {
+      id: uuidv4(),
+      ...data,
+      imageOfProduct:
+        "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png",
+    };
+    const response = await axiosWithConfig.post("/products", newData);
+    return response.data
+  } catch (error) {
+    throw Error("Failed to create a product")
+  }
+};
+
+export const updateProduct = async (data) => {
+  const { id } = data;
+  try {
+    const newData = {
+      id: uuidv4(),
+      ...data,
+      imageOfProduct:
+        "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png",
+    };
+    const response = await axiosWithConfig.put(`/products/${id}`, newData);
+    return response.data
+  } catch (error) {
+    throw Error("Failed to update a product")
+  }
+};
+
+export const deleteProduct = async (id) => {
+  try {
+    const response = await axiosWithConfig.delete("/products/" + id);
+    return response.data;
+  } catch (error) {
+    throw Error("Failed to get a product");
+  }
+};
