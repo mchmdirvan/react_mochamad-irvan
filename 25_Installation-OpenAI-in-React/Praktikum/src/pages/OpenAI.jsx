7abc9d31dd12a86bd56e33d44b0b6535644d0a84
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-15 11:52:00 +0700

    feat: create configuration openAI

--- 25_Installation-OpenAI-in-React/Praktikum/src/pages/OpenAI.jsx
+++ 25_Installation-OpenAI-in-React/Praktikum/src/pages/OpenAI.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import Navbar from '../components/Navbar'
-import { useState } from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { useState } from "react";
+import OpenAI from "openai";
 
-export default function OpenAI() {
-  return (
-    <div>OpenAI</div>
-  )
+const configuration = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+export default function IndexOpenAI() {
+  return <div>OpenAI</div>;
 }
