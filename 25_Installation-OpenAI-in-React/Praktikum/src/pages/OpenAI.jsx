a1e7147067be567a5fa938017ff3487dcba7a2f4
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-15 19:48:40 +0700

    bug: console or input prompt state not appear

--- 25_Installation-OpenAI-in-React/Praktikum/src/pages/OpenAI.jsx
+++ 25_Installation-OpenAI-in-React/Praktikum/src/pages/OpenAI.jsx
@@ -4,8 +4,9 @@ import React from "react";
 import OpenAI from "openai";
 import clsx from "clsx";
 
+import { useTitle } from "../utils/hooks/customHooks";
 import { Input } from "../components/FormComponent";
-import  Button  from "../components/Button";
+import Button from "../components/Button";
 import Navbar from "../components/Navbar";
 
 const configuration = new OpenAI({
@@ -13,83 +14,33 @@ const configuration = new OpenAI({
   dangerouslyAllowBrowser: true,
 });
 
-
 export default function IndexOpenAI() {
+  useTitle("Open AI");
   const [prompt, setPrompt] = useState("");
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  async function fetchData() {
-    try {
-      const response = await configuration.chat.completions.create({
-        messages: [{ role: "system", content: "You are a helpful assistant." }],
-        model: "gpt-3.5-turbo",
-      });
-      setResults(response.choices);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
-  const handleSubmit = async (event) => {
-    setIsLoading(true);
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const userMsg = {
-      message: {
-        content: prompt,
-        role: "user",
-      },
-    };
-    const newData = [...results, userMsg];
-    setResults(newData);
-    setPrompt("");
-    try {
-      const response = await configuration.chat.completions.create({
-        messages: [{ role: "user", content: prompt }],
-        model: "gpt-3.5-turbo",
-      });
-      const choice = response.choices[0];
-      setResults([...newData, choice]);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
+    console.log(prompt);
   };
+
   return (
     <>
-      <div className="grow flex flex-col overflow-auto">
-        {results.map((result) => (
-          <p
-            className={clsx(
-              "border rounded-xl p-3 mb-4 w-fit",
-              result.message.role === "assistant" ? "self-start" : "self-end"
-            )}
-            key={result.message.content}
-          >
-            {result.message.content}
-          </p>
-        ))}
+      <Navbar />
+      <div className="w-full h-[90vh] py-4 px-8 flex flex-col text-black">
+        <form onSubmit={handleSubmit} className="flex items-center gap-3">
+          <Input
+            placeholder="Insert prompt"
+            onChange={(event) => setPrompt(event.target.value)}
+            className="w-[90vw]"
+            name="insertPrompt"
+            id="insertPrompt"
+            ariaLabel="insert-prompt"
+          />
+          <Button type="submit" label="submit" className="px-3" ariaLabel="btn-submit" id="btn-submit" />
+        </form>
       </div>
-      <form onSubmit={handleSubmit} className="flex items-center gap-3">
-        <Input
-          placeholder="Insert prompt"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-        <Button
-          label={isLoading ? "Loading" : "Submit"}
-          type="submit"
-          disabled={isLoading}
-          aria-disabled={isLoading}
-        />
-      </form>
     </>
-  )
+  );
 }
