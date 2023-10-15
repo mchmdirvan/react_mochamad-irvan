eb99d574c6a9af1f33f3f5508a8d686c7220b01e
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-15 11:30:19 +0700

    feat: create openAI jsx pages

---
+++ 25_Installation-OpenAI-in-React/Praktikum/src/pages/OpenAI.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { useState } from 'react'
+
+export default function OpenAI() {
+  return (
+    <div>OpenAI</div>
+  )
+}
