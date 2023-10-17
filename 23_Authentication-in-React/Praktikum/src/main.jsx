ddfdfefb0d89230729b98bd95b68410e079a87b5
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-10-17 16:30:56 +0700

    feat: Integrate TokenProvider for context in the root component

--- 23_Authentication-in-React/Praktikum/src/main.jsx
+++ 23_Authentication-in-React/Praktikum/src/main.jsx
@@ -4,13 +4,16 @@ import { Provider } from "react-redux";
 import React from "react";
 
 import store from "./utils/states/redux/store/store";
-import "./styles/index.css";
 import App from "./routes/index";
+import "./styles/index.css";
+import {TokenProvider} from './utils/states/contexts/token-context'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
+    <TokenProvider>
       <App />
+    </TokenProvider>
     </Provider>
   </React.StrictMode>
 );
